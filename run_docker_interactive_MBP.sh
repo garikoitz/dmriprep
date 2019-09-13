3b29d4c95c3f724d183e8a0402db95859749cf32
Garikoitz <garikoitz@gmail.com>
2019-09-12 18:59:05 -0700

    first commit

---
+++ run_docker_interactive_MBP.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -v /Users/glerma/Documents/STANFORD_PROJECTS/dmriprep:/data:ro -v /Users/glerma/Documents/STANFORD_PROJECTS/dmriprep/dockerOut:/out -v $HOME/soft/dmriprep/dmriprep:/usr/local/miniconda/lib/python3.7/site-packages/dmriprep:ro --entrypoint=bash nipreps/dmriprep:latest
