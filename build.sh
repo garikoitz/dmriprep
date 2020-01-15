7b5fc8593a53d85def017c083b6b34b6c554cc48
garikoitz <garikoitz@gmail.com>
2020-01-15 12:04:37 -0800

    clean local

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker build --rm -t nipreps/dmriprep:latest \
-             --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-             --build-arg VCS_REF=`git rev-parse --short HEAD` \
-             --build-arg VERSION=$( python get_version.py ) .
--- run_docker_interactive_BLACK.sh
+++
@@ -1,6 +0,0 @@
-docker run -it --rm -v ~/TESTDATA/dmriprep/openNeuroTraveling2/THP002:/data:ro \
-    -v ~/TESTDATA/dmriprep/openNeuroTraveling2/dockerOut:/out \
-    -v $HOME/soft/dmriprep/dmriprep:/usr/local/miniconda/lib/python3.7/site-packages/dmriprep:ro \
-    -v $FREESURFER_HOME/license.txt:/opt/freesurfer/license.txt:ro \
-    --entrypoint=bash \
-    nipreps/dmriprep:latest 
